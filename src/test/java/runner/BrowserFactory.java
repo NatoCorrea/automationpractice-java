fb488d13bc8c5134906a8cef18177b3436f61bf3
Nato Correa <renatojpcorrea@gmail.com>
2025-03-24 23:27:30 -0300

    Atualização do readme e criação da classe BrowserFactory

---
+++ src/test/java/runner/BrowserFactory.java
@@ -0,0 +1,15 @@
+package runner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BrowserFactory {
+
+    public static WebDriver getBrowser() {
+
+        WebDriver navegador;
+        ChromeOptions options;
+
+        System
+    }
+}
